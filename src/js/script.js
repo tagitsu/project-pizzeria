1555369c759c9239b8b2814b8408973fc542390b
Julia Diaków <tagitsu@gmail.com>
2022-10-25 13:38:46 +0200

    Add json server to task-runner and move there products data

--- src/js/script.js
+++ src/js/script.js
@@ -70,6 +70,11 @@
     cart: {
       defaultDeliveryFee: 20,
     },
+    db: {
+      url: '//localhost:3131',
+      products: 'products',
+      orders: 'orders',
+    },
   };
 
   const templates = {
@@ -304,7 +309,7 @@
       thisCartProduct.dom.remove.addEventListener('click', function() {
         event.preventDefault();
         thisCartProduct.remove();
-      })
+      });
     }
   }
 
@@ -314,14 +319,32 @@
       const thisApp = this;
       
       for (let productData in thisApp.data.products) {
-        new Product(productData, thisApp.data.products[productData]);
+        new Product(thisApp.data.products[productData].id, thisApp.data.products[productData]);
       }
     },
 
     initData: function() {
       const thisApp = this;
 
-      thisApp.data = dataSource;
+      thisApp.data = {};
+
+      const url = settings.db.url + '/' + settings.db.products;
+
+      fetch(url) 
+        .then(function(rawResponse) {
+          return rawResponse.json();
+        })
+        .then(function(parsedResponse) {
+          console.log('parsedResponse', parsedResponse);
+
+        // save parsedResponse as thisApp.data.products
+        thisApp.data.products = parsedResponse;  
+        // execute initMenu method
+        thisApp.initMenu();  
+
+      });
+
+      console.log('thisApp.data', JSON.stringify(thisApp.data));
     },
 
     initCart: function() {
@@ -335,7 +358,6 @@
       const thisApp = this;
 
       thisApp.initData();
-      thisApp.initMenu();
       thisApp.initCart();
     },
   };
