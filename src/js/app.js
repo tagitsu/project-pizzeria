e7430a72aa089c122ae3f97bdf1c34737cbc5243
Julia Diaków <tagitsu@gmail.com>
2022-10-29 14:02:23 +0200

    Add initPages method to app (adding subpages) - 9.4 video

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,56 @@
-import {settings, select} from './settings.js';
+import {settings, select, classNames} from './settings.js';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
 
 
 const app = {
-  
+  initPages: function() {
+    const thisApp = this;
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);
+    const idFromHash = window.location.hash.replace('#/', '');
+
+    let pageMatchingHash = thisApp.pages[0].id; 
+    for (let page of thisApp.pages) {
+      if (page.id == idFromHash) {
+        pageMatchingHash = page.id;
+        break;
+      }
+    }
+
+    thisApp.activatePage(pageMatchingHash);
+
+    for (let link of thisApp.navLinks) {
+      link.addEventListener('click', function(event) {
+        const clickedElement = this;
+        event.preventDefault();
+
+        // get id from href attribute
+        const id = clickedElement.getAttribute('href').replace('#', '');
+        // run activatePage method with that id
+        thisApp.activatePage(id);
+        // change URL hash
+        window.location.hash = '#/' + id;
+      });
+    }
+
+  },
+
+  activatePage: function(pageId) {
+    const thisApp = this;
+    // add class active to matched subpage, and remove class active from non-matched subpage
+    for (let page of thisApp.pages) {
+      page.classList.toggle(classNames.pages.active, page.id == pageId);
+    }
+    // add class active to matched subpage link, and remove class active from non-matched subpage link
+    for (let link of thisApp.navLinks) {
+      link.classList.toggle(
+        classNames.nav.active, 
+        link.getAttribute('href') == '#' + pageId
+      );
+    }
+  },
+
   initMenu: function(){
     const thisApp = this;
     
@@ -49,7 +95,7 @@ const app = {
 
   init: function() {
     const thisApp = this;
-
+    thisApp.initPages();
     thisApp.initData();
     thisApp.initCart();
   },
