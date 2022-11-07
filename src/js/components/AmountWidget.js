2eb44be37dd64c500bd34e4de0b32b3e8314c274
Julia Diaków <tagitsu@gmail.com>
2022-11-07 13:21:40 +0100

    Fix display start value of product

--- src/js/components/AmountWidget.js
+++ src/js/components/AmountWidget.js
@@ -14,6 +14,7 @@ class AmountWidget extends BaseWidget {
     const thisWidget = this;
 
     thisWidget.dom.input = thisWidget.dom.wrapper.querySelector(select.widgets.amount.input);
+    console.log('czy to jest input', this.dom.input);
     thisWidget.dom.linkDecrease = thisWidget.dom.wrapper.querySelector(select.widgets.amount.linkDecrease);
     thisWidget.dom.linkIncrease = thisWidget.dom.wrapper.querySelector(select.widgets.amount.linkIncrease);
   }
